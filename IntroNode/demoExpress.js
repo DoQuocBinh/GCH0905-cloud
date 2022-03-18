ab2b8c0b800836fc54cf11d1b8b86642e2949962
DoQuocBinh <binhdq@fpt.edu.vn>
2022-03-18 11:53:33 +0700

    express

---
+++ IntroNode/demoExpress.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+
+//trang home
+app.get('/',(req,res)=>{
+    res.writeHead(200,{'content-Type' : 'text/html'})
+    res.write('<img src="images/ipad.jpg"/>')
+    res.write("<a href='/about'>About</a><br>")
+    res.end("Home Page")
+})
+
+//trang about
+app.get('/about',(req,res)=>{
+    res.writeHead(200,{'content-Type' : 'text/html'})
+    res.write("<a href='/'>Home</a><br>")
+    res.end("About Page")
+})
+
+const PORT =5000
+app.listen(PORT)
+console.log("Server is running!")
