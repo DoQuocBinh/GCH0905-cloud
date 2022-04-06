a05d6ce4833db22bb2dbada86f2dfbf7ba8ee0a5
DoQuocBinh <binhdq@fpt.edu.vn>
2022-04-06 10:38:54 +0700

    refactor entity

--- NodeMongoose/app.js
+++ NodeMongoose/app.js
@@ -15,9 +15,7 @@ app.get('/new',(req,res)=>{
 })
 
 app.get('/viewAll', (req,res)=>{
-    var mongoDB = 'mongodb://tommy:123456789mu@cluster0-shard-00-00.lkrga.mongodb.net:27017,cluster0-shard-00-01.lkrga.mongodb.net:27017,cluster0-shard-00-02.lkrga.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
-    mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
-    var db = mongoose.connection;
+   
 
     const query =  Product.find({},(err,result)=>{
         if(err)
@@ -37,9 +35,9 @@ app.post('/newProduct',async (req,res)=>{
     const price  = req.body.txtPrice
     const picURL  = req.body.txtPic
     
-    var mongoDB = 'mongodb://tommy:123456789mu@cluster0-shard-00-00.lkrga.mongodb.net:27017,cluster0-shard-00-01.lkrga.mongodb.net:27017,cluster0-shard-00-02.lkrga.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
-    mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
-    var db = mongoose.connection;
+    // var mongoDB = 'mongodb://tommy:123456789mu@cluster0-shard-00-00.lkrga.mongodb.net:27017,cluster0-shard-00-01.lkrga.mongodb.net:27017,cluster0-shard-00-02.lkrga.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
+    // mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+    // var db = mongoose.connection;
     const productEntity = new Product({'name':name,'price':price,'picURL':picURL})
     await productEntity.save()
     res.redirect('/')
--- NodeMongoose/models/Product.js
+++ NodeMongoose/models/Product.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+var mongoDB = 'mongodb://tommy:123456789mu@cluster0-shard-00-00.lkrga.mongodb.net:27017,cluster0-shard-00-01.lkrga.mongodb.net:27017,cluster0-shard-00-02.lkrga.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+
 const ProductSchema = new Schema({
     name : {type:String, required:true},
     price: {type:Number},
