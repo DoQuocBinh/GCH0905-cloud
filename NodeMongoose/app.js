36bda406905665f1d5ab40ce57afc71b21e000ad
DoQuocBinh <binhdq@fpt.edu.vn>
2022-04-06 10:34:08 +0700

    update viewall

--- NodeMongoose/app.js
+++ NodeMongoose/app.js
@@ -15,14 +15,19 @@ app.get('/new',(req,res)=>{
 })
 
 app.get('/viewAll', (req,res)=>{
+    var mongoDB = 'mongodb://tommy:123456789mu@cluster0-shard-00-00.lkrga.mongodb.net:27017,cluster0-shard-00-01.lkrga.mongodb.net:27017,cluster0-shard-00-02.lkrga.mongodb.net:27017/test?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin';
+    mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+    var db = mongoose.connection;
+
     const query =  Product.find({},(err,result)=>{
         if(err)
             console.log(err)
-        else
-            result.forEach(element => {
-            console.log(element.name)
-            });
+        else{
+            // result.forEach(element => {
+            // console.log(element.name)
+            // });
             res.render('allProduct',{'products':result})
+        }            
     })
     
 })
