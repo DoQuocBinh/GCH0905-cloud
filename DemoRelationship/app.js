f871b6f297308ff1efaeca1beef4dd870de28ea9
DoQuocBinh <binhdq@fpt.edu.vn>
2022-04-13 09:24:57 +0700

    insert story and author

--- DemoRelationship/app.js
+++ DemoRelationship/app.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const async = require('hbs/lib/async')
 const Author = require('./models/Author')
+const Story = require('./models/Story')
 const app = express()
 
 app.set('view engine','hbs')
 app.use(express.urlencoded({extended:true}))
 
-app.get('/',(req,res)=>{
-    res.render('home')
+app.get('/',async (req,res)=>{
+    const query = await Author.find()
+    res.render('home',{'author':query})
+})
+
+app.post('/newStory',async (req,res)=>{
+    const title = req.body.txtStory
+    const authorId = req.body.author
+
+    var story = new Story({'title':title,author:authorId})
+    await story.save()
+    console.log(story._id)
+    res.redirect('/')
 })
 
 app.post('/newAuthor', async (req,res)=>{
